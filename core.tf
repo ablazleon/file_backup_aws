b740505ffd880476b531936c74f55393c1e797f9
a838102 <adrian.blazquez@atos.net>
2022-04-26 16:04:27 +0200

    Proper reference to arn

--- core.tf
+++ core.tf
@@ -3,7 +3,7 @@ resource "aws_datasync_agent" "datasync_agent" {
   ip_address = aws_instance.ds-agent_tf.public_ip
   name       = "datasync_agent"
   # Depende que se cree la conexión con el agente de que se haya creado la vm agente
-  depends_on = [ aws_instance.ds-agent_tf ]
+  depends_on = [aws_instance.ds-agent_tf]
 }
 
 # Se crea un task de migración para lo que es necesario una lcoalización nfs y otra s3
@@ -32,7 +32,7 @@ resource "aws_datasync_location_s3" "core_bucket_loc_tf" {
   s3_config {
     bucket_access_role_arn = aws_iam_role.datasync_to_s3_role.arn
   }
-  depends_on = [ aws_datasync_agent.datasync_agent, aws_s3_bucket.core_bucket_tf, aws_iam_role.datasync_to_s3_role ]
+  depends_on = [aws_datasync_agent.datasync_agent, aws_s3_bucket.core_bucket_tf, aws_iam_role.datasync_to_s3_role]
 }
 
 resource "aws_iam_role" "datasync_to_s3_role" {
@@ -41,19 +41,19 @@ resource "aws_iam_role" "datasync_to_s3_role" {
   # Terraform's "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
   assume_role_policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
+    "Version" : "2012-10-17",
+    "Statement" : [
       {
-        "Action": [
+        "Action" : [
           "s3:GetBucketLocation",
           "s3:ListBucket",
           "s3:ListBucketMultipartUploads"
         ],
-        "Effect": "Allow",
-        "Resource": "${aws_s3_bucket.core_bucket_tf}"
+        "Effect" : "Allow",
+        "Resource" : "${aws_s3_bucket.core_bucket_tf.arn}"
       },
       {
-        "Action": [
+        "Action" : [
           "s3:AbortMultipartUpload",
           "s3:DeleteObject",
           "s3:GetObject",
@@ -62,8 +62,8 @@ resource "aws_iam_role" "datasync_to_s3_role" {
           "s3:PutObjectTagging",
           "s3:PutObject"
         ],
-        "Effect": "Allow",
-        "Resource": "${aws_s3_bucket.core_bucket_tf}/*"
+        "Effect" : "Allow",
+        "Resource" : "${aws_s3_bucket.core_bucket_tf.arn}/*"
       }
     ]
   })
--- datacenter.tf
+++ datacenter.tf
@@ -1,12 +1,12 @@
 
 resource "aws_instance" "server_tf" {
-  ami           = "ami-0c6ebbd55ab05f070"
-  instance_type = "t2.micro"
+  ami                         = "ami-0c6ebbd55ab05f070"
+  instance_type               = "t2.micro"
   key_name                    = aws_key_pair.server-key.key_name
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.sg.id]
   subnet_id                   = aws_subnet.subnet.id
-  user_data = <<EOF
+  user_data                   = <<EOF
           #! /bin/bash
           sudo apt-get update
           EOF
@@ -16,8 +16,8 @@ resource "aws_instance" "server_tf" {
 }
 
 resource "aws_instance" "ds-agent_tf" {
-  ami           = "ami-099ed32be5f65d949"
-  instance_type = "m5.2xlarge"
+  ami                         = "ami-099ed32be5f65d949"
+  instance_type               = "m5.2xlarge"
   key_name                    = aws_key_pair.server-key.key_name
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.sg.id]
