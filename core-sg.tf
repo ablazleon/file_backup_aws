0b91f74812fb214d2a33fc8995abf5d42d54a6cc
a838102 <adrian.blazquez@atos.net>
2022-04-26 23:07:56 +0200

    Se cambia el rol porque sino no seja montar

--- core-sg.tf
+++ core-sg.tf
@@ -14,9 +14,9 @@ resource "aws_storagegateway_gateway" "sg_tf" {
 resource "aws_storagegateway_nfs_file_share" "nfs_fs_tf" {
   client_list  = ["0.0.0.0/0"]
   gateway_arn  = aws_storagegateway_gateway.sg_tf.arn
-  location_arn = aws_s3_bucket.core_bucket_tf.arn
+  location_arn = "${aws_s3_bucket.core_bucket_tf.arn}/migration/"
   role_arn     = aws_iam_role.sg_s3_role.arn
-  file_share_name = "/migration/"
+  file_share_name = "nfs_fs_tf"
 
   timeouts {
     create = "1m"
@@ -37,8 +37,11 @@ resource "aws_iam_role" "sg_s3_role" {
       "Statement" : [
         {
           "Action" : [
+            "s3:GetAccelerateConfiguration",
             "s3:GetBucketLocation",
+            "s3:GetBucketVersioning",
             "s3:ListBucket",
+            "s3:ListBucketVersions",
             "s3:ListBucketMultipartUploads"
           ],
           "Effect" : "Allow",
@@ -48,11 +51,13 @@ resource "aws_iam_role" "sg_s3_role" {
           "Action" : [
             "s3:AbortMultipartUpload",
             "s3:DeleteObject",
+            "s3:DeleteObjectVersion",
             "s3:GetObject",
+            "s3:GetObjectAcl",
+            "s3:GetObjectVersion",
             "s3:ListMultipartUploadParts",
-            "s3:GetObjectTagging",
-            "s3:PutObjectTagging",
-            "s3:PutObject"
+            "s3:PutObject",
+            "s3:PutObjectAcl"
           ],
           "Effect" : "Allow",
           "Resource" : "${aws_s3_bucket.core_bucket_tf.arn}/*"
