0577d078ecab7b21d9efbb361b12dc00bd047ab9
a838102 <adrian.blazquez@atos.net>
2022-04-27 10:33:43 +0200

    Forzar el vaciado del bucket

--- core-bucket.tf
+++ core-bucket.tf
@@ -1,9 +1,11 @@
 
 
 # Se crea un task de migración para lo que es necesario una lcoalización nfs y otra s3
-
+# Forzar la destrucción del bucket
+# https://medium.com/interleap/terraform-destroy-replace-buckets-cf9d63d0029d
 resource "aws_s3_bucket" "core_bucket_tf" {
   bucket = "core-bucket-tf"
+  force_destroy = true
 
   tags = {
     Name = "core-bucket-tf"
