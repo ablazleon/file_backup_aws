a3ad9266de51b7941f68ba814c0f3067fd5f6995
a838102 <adrian.blazquez@atos.net>
2022-04-28 19:40:02 +0200

    Upgrade to version 4

--- core-bucket.tf
+++ core-bucket.tf
@@ -4,16 +4,20 @@
 # Forzar la destrucción del bucket
 # https://medium.com/interleap/terraform-destroy-replace-buckets-cf9d63d0029d
 # Crear un folder
-# # https://stackoverflow.com/questions/37491893/how-to-create-a-folder-in-an-amazon-s3-bucket-using-terraform
+
 resource "aws_s3_bucket" "core_bucket_tf" {
   bucket = "core-bucket-tf"
-  key = "migration/"
-  content_type = "application/x-directory"
   force_destroy = true
-
   tags = {
     Name = "core-bucket-tf"
   }
+}
+
+# Se crea un objeto con una carpeta
+# https://stackoverflow.com/questions/37491893/how-to-create-a-folder-in-an-amazon-s3-bucket-using-terraform
+resource "aws_s3_object" "core_bucket_folder_tf" {
+  bucket = aws_s3_bucket.core_bucket_tf.id
+  key    = "migration/"
 }
 
 resource "aws_s3_bucket_acl" "core_bucket_acl_tf" {
