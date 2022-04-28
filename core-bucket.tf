1a7fdb58e7cb37148ea8e34cd6af853819ea4c42
a838102 <adrian.blazquez@atos.net>
2022-04-28 19:20:50 +0200

    Sólo con key da error, se añade contnet

--- core-bucket.tf
+++ core-bucket.tf
@@ -3,9 +3,12 @@
 # Se crea un task de migración para lo que es necesario una lcoalización nfs y otra s3
 # Forzar la destrucción del bucket
 # https://medium.com/interleap/terraform-destroy-replace-buckets-cf9d63d0029d
+# Crear un folder
+# # https://stackoverflow.com/questions/37491893/how-to-create-a-folder-in-an-amazon-s3-bucket-using-terraform
 resource "aws_s3_bucket" "core_bucket_tf" {
   bucket = "core-bucket-tf"
-  //prefix = "migration"
+  key = "migration/"
+  content_type = "application/x-directory"
   force_destroy = true
 
   tags = {
