f15e234f564c8b2884e3f6f0e0928c4b1801f31f
a838102 <adrian.blazquez@atos.net>
2022-05-31 18:54:48 +0200

    create two modules and instaceate them

---
+++ modules/datacenter/outputs.tf
@@ -0,0 +1,19 @@
+output "Server-Public-IP" {
+  value = aws_instance.server_tf.public_ip
+}
+
+output "DS-Agent-Public-IP" {
+  value = aws_instance.ds-agent_tf.public_ip
+}
+
+output "SG-Agent-Public-IP" {
+  value = aws_instance.sg-agent_tf.public_ip
+}
+
+output "NFSServer-Private-IP" {
+  value = aws_instance.server_tf.private_ip
+}
+
+output "ebs-device-name" {
+  value = aws_volume_attachment.ebs_att_tf.device_name
+}
---
+++ modules/datacenter/variables.tf
@@ -0,0 +1,5 @@
+variable "file_name" {
+  description = "file_name of the testing file"
+  type        = string
+  default     = "hola"
+}
