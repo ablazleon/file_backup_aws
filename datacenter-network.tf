c66331e3f814aab56b2718c4633898249f7ce71b
a838102 <adrian.blazquez@atos.net>
2022-05-31 13:14:55 +0200

    Adding the key

--- datacenter-network.tf
+++ datacenter-network.tf
@@ -3,7 +3,7 @@
 #Create key-pair for logging into EC2
 resource "aws_key_pair" "server-key" {
   key_name   = "id_rsa"
-  public_key = file("~/.ssh/id_rsa.pub")
+  public_key = file(".ssh/id_rsa.pub")
 }
 
 #Create VPC
